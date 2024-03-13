979dbd5b0b2951a6de2230e57d087ba38da5b62d
alawealwosh <alawe.alwosh@gmail.com>
2024-03-13 10:48:31 +0300

    update code for new predicitons

--- pred_main_github.py
+++ pred_main_github.py
@@ -1,26 +1,28 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from datetime import datetime
+current_dateTime = datetime.now()
+
+formatted_datetime = current_dateTime.strftime("%Y-%m-%d_%H-%M-%S")
 
 # Load dataset
-df = pd.read_excel('Dataset_of_Diabetes_Iraq.xlsx')
+df = pd.read_excel('d_1.xlsx')
 
-# Label encode the target variable
-label_encoder = LabelEncoder()
-df['CLASS'] = label_encoder.fit_transform(df['CLASS'])
-df['Gender'] = label_encoder.fit_transform(df['Gender'])
 
-X = df.drop('CLASS', axis=1)  # Replace 'TargetColumn' with the name of your target column
-y = df['CLASS']
+X = df.drop('Outcome', axis=1)  # Replace 'TargetColumn' with the name of your target column
+y = df['Outcome']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Standardize numerical features
+
+
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
+
 
 model = Sequential([
     Dense(16, activation='relu', input_shape=(X_train_scaled.shape[1],)),
@@ -39,13 +41,9 @@ print(f"Test accuracy: {accuracy*100:.2f}%")
 
 # Assuming new_data is your new Excel data loaded and preprocessed (without the target variable)
 
-new_data = pd.read_excel('Dataset_of_Diabetes_Iraq_No_Class.xlsx')
 
-# One-hot encode categorical columns in the new data
-# new_data = pd.get_dummies(new_data, columns=['Gender'])
-# new_data['CLASS'] = label_encoder.fit_transform(new_data['CLASS'])
-new_data['Gender'] = label_encoder.fit_transform(new_data['Gender'])
 
+new_data = pd.read_excel('d_2.xlsx')
 
 new_data_scaled = scaler.transform(new_data)
 predictions = model.predict(new_data_scaled)
@@ -53,13 +51,10 @@ predictions = model.predict(new_data_scaled)
 # Convert predictions to labels (e.g., 0 and 1)
 predicted_labels = (predictions > 0.5).astype(int)
 
-# Map predictions back to original class labels
-predicted_labels = label_encoder.inverse_transform(predicted_labels.flatten())
-
-predictions_df = pd.DataFrame(predicted_labels, columns=['Predicted_Class'])
+predictions_df = pd.DataFrame(predicted_labels, columns=['Predicted_Outcome'])
 
 # Concatenate original DataFrame with predictions DataFrame
 result_df = pd.concat([df, predictions_df], axis=1)
 
 # Export the result to a new Excel file
-result_df.to_excel('predicted_results__Iraqi_hospital.xlsx', index=False)
+result_df.to_excel(f'predicted_results_date_{formatted_datetime}.xlsx', index=False)
